a09213af092d0969cbccfdea50ec0ab724a1f9af
hmweb77 <oussamahamdani1@gmail.com>
2024-11-19 14:53:10 +0000

    add all changes v2

--- src/sanityClient.ts
+++ src/sanityClient.ts
@@ -3,7 +3,7 @@
 import { createClient } from '@sanity/client';
 
 export const sanityClient = createClient({
-  projectId: "quaxgg3z", // Replace with your actual project ID
+  projectId: "5ngontx0", // Replace with your actual project ID
   dataset: "production", // Or your specific dataset
   apiVersion: '2023-11-03', // Today's date or the preferred API version
   useCdn: false, 
