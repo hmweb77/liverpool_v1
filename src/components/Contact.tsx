923934be7aef607a9390094440e5ad6ebd8e5c26
hmweb77 <oussamahamdani1@gmail.com>
2024-12-12 23:59:40 +0000

    update translation and submit and reservation v2

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -146,7 +146,7 @@ const Contact = () => {
               <input type="hidden" name="_subject" value="New Event Request - Liverpool Bar" />
               <input type="hidden" name="_template" value="table" />
               <input type="hidden" name="_captcha" value="false" />
-              <input type="hidden" name="_next" value={window.location.href} />
+              {/* <input type="hidden" name="_next" value={window.location.href} /> */}
 
               <div>
                 <label className="block text-sm font-medium text-gray-300 mb-2">
